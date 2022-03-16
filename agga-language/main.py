c38b96dfe01e64e1fbb5f4e8a570c832b41398bd
HanZawNyine <hanzawnyine143@gmail.com>
2022-03-16 21:00:49 +0630

    agga lanuage wnd

--- agga-language/main.py
+++ agga-language/main.py
@@ -1,41 +1,14 @@
-from lib.Intepreter import *
+from lib import Interpreter
 
-#######################################
-# RUN
-#######################################
+while True:
+	text = input('basic > ')
+	if text.strip() == "": continue
+	result, error = Interpreter.run('<stdin>', text)
 
-global_symbol_table = SymbolTable()
-global_symbol_table.set("NULL", Number(0))
-global_symbol_table.set("FALSE", Number(0))
-global_symbol_table.set("TRUE", Number(1))
-
-
-def run(fn, text):
-    # Generate tokens
-    lexer = Lexer(fn, text)
-    tokens, error = lexer.make_tokens()
-    if error: return None, error
-
-    # Generate AST
-    parser = Parser(tokens)
-    ast = parser.parse()
-    if ast.error: return None, ast.error
-
-    # Run program
-    interpreter = Interpreter()
-    context = Context('<program>')
-    context.symbol_table = global_symbol_table
-    result = interpreter.visit(ast.node, context)
-
-    return result.value, result.error
-
-
-if __name__ == "__main__":
-    while True:
-        text = input('Agga-Capitalize > ')
-        result, error = run('<stdin>', text)
-
-        if error:
-            print(error.as_string())
-        elif result:
-            print(result)
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
