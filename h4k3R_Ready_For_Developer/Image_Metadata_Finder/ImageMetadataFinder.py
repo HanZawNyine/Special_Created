9fca79715f4773a2c19eb2879107f20bba2ae466
HanZawNyine <hanzawnyine143@gmail.com>
2022-03-21 16:14:12 +0630

    firefox and chrome password extractor and image metadata

---
+++ h4k3R_Ready_For_Developer/Image_Metadata_Finder/ImageMetadataFinder.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+
+class imageMetaData:
+    def __init__(self, imageName):
+        self.imageName = imageName
+
+    def extract(self):
+        # imagename = "image.jpg"  # sys.argv[1]
+        image = Image.open(self.imageName)
+
+        # extract EXIF data
+        exifdata = image.getexif()
+
+        # iterating over all EXIF data fields
+        for tag_id in exifdata:
+            # get the tag name, instead of human unreadable tag id
+            tag = TAGS.get(tag_id, tag_id)
+            data = exifdata.get(tag_id)
+            # decode bytes
+            if isinstance(data, bytes):
+                data = data.decode()
+            print(f"{tag:25}: {data}")
+
+
+if __name__ == '__main__':
+    imageMetaData = imageMetaData('image.jpg')
+    imageMetaData.extract()
